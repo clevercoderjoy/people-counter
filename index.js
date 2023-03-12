46c1e2a6137687dea029e6a88cddc993dd91af46
Shaheen-Siddiqui <shanvisiddya@gmail.com>
2023-03-12 17:13:05 +0530

    fix: counter is no more changing it's value on clicking save button

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const displayCounter = document.getElementById("count-el");
 const btnIncrement = document.getElementById("increment-btn");
 const saveCounter = document.getElementById("save-el");
 const btnSave = document.getElementById("save-btn");
+const resetCounter = document.getElementById("reset-counter");
+
 let counter = 0;
 
 const incrementCounter = () => {
@@ -11,8 +13,17 @@ const incrementCounter = () => {
 btnIncrement.addEventListener("click", incrementCounter);
 
 const saveCurrentCounter = () => {
+if (counter!=0) {
     saveCounter.textContent += ` - ${counter}`;
+}else{
+    alert("counter is zero(0) Click the INCREMENT Button then save")
+}
+
+}
+const ResetCounterAsZero=()=>{
+
     counter = 0;
     displayCounter.textContent = `${counter}`;
 }
+resetCounter.addEventListener("click",ResetCounterAsZero )
 btnSave.addEventListener("click", saveCurrentCounter);
