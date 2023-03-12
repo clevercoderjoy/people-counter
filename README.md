913650c352e024462b52f79e79d1be93fe04f39b
clevercoderjoy <clevercoderjoy@gmail.com>
2023-03-12 14:38:44 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# people-counter
+ A web app to count people
---
+++ index.js
@@ -0,0 +1,18 @@
+const displayCounter = document.getElementById("count-el");
+const btnIncrement = document.getElementById("increment-btn");
+const saveCounter = document.getElementById("save-el");
+const btnSave = document.getElementById("save-btn");
+let counter = 0;
+
+const incrementCounter = () => {
+    counter += 1;
+    displayCounter.textContent = `${counter}`;
+}
+btnIncrement.addEventListener("click", incrementCounter);
+
+const saveCurrentCounter = () => {
+    saveCounter.textContent += ` ${counter} - `;
+    counter = 0;
+    displayCounter.textContent = `${counter}`;
+}
+btnSave.addEventListener("click", saveCurrentCounter);
